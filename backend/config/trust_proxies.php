fd3f6010edaf20e3caf2e881658cd674d688c294
leon <zhaoyangr@gmail.com>
2025-08-16 20:35:38 +0800

    fix the command already remove in larvel

--- backend/config/trust_proxies.php
+++ backend/config/trust_proxies.php
@@ -2,29 +2,11 @@
 use Illuminate\Http\Request;
 
 return [
-    /*
-    |--------------------------------------------------------------------------
-    | Trusted Proxies
-    |--------------------------------------------------------------------------
-    |
-    | Set the trusted proxies for your application. This is useful when your
-    | application is behind a load balancer or reverse proxy, ensuring that
-    | Laravel correctly detects the client's IP address and HTTPS status.
-    |
-    */
-
     'proxies' => '*', // Trust all proxies
 
-    /*
-    |--------------------------------------------------------------------------
-    | Headers for Trusted Proxies
-    |--------------------------------------------------------------------------
-    |
-    | These headers are used to detect proxy-related information. The default
-    | value is suitable for most applications, but you can customize it if
-    | needed.
-    |
-    */
-
-    'headers' => Illuminate\Http\Request::HEADER_X_FORWARDED_ALL,
+    'headers' => Request::HEADER_X_FORWARDED_FOR |
+                 Request::HEADER_X_FORWARDED_HOST |
+                 Request::HEADER_X_FORWARDED_PROTO |
+                 Request::HEADER_X_FORWARDED_PORT |
+                 Request::HEADER_X_FORWARDED_TRAFFIC,
 ];
